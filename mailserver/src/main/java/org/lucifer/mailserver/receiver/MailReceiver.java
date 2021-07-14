2bf449cbef5792376d54ae717944fd66ad7111a4
lucifer <xiaoweliang@qq.com>
2021-07-14 20:44:51 +0800

    利用Redis防止消息重复消费

--- mailserver/src/main/java/org/lucifer/mailserver/receiver/MailReceiver.java
+++ mailserver/src/main/java/org/lucifer/mailserver/receiver/MailReceiver.java
@@ -1,20 +1,26 @@
 package org.lucifer.mailserver.receiver;
 
+import com.rabbitmq.client.Channel;
 import org.lucifer.vbluciferpro.model.Employee;
 import org.lucifer.vbluciferpro.model.MailConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.Date;
 
 @Component
@@ -28,11 +34,22 @@ public class MailReceiver {
     MailProperties mailProperties;
     @Autowired
     TemplateEngine templateEngine;
+    @Autowired
+    StringRedisTemplate redisTemplate;
 
-
-    @RabbitListener(queues = MailConstants.MAIL_QUEUE_NAME)
-    public void handler(Employee employee){
-        logger.info(employee.toString());
+    @RabbitListener(queues =  MailConstants.MAIL_QUEUE_NAME)
+    public void handler(Message message, Channel channel) throws IOException {
+        //利用redis完成消息消费
+        Employee employee = (Employee) message.getPayload();
+        MessageHeaders headers = message.getHeaders();
+        Long tag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        String msgId = (String) headers.get("spring_returned_message_correlation");
+        if (redisTemplate.opsForHash().entries("mail_log").containsKey(msgId)) {
+            //redis 中包含该 key，说明该消息已经被消费过
+            logger.info(msgId + ":消息已经被消费");
+            channel.basicAck(tag, false);//确认消息已消费
+            return;
+        }
         //收到消息，发送邮件
         MimeMessage msg = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(msg);
@@ -52,13 +69,14 @@ public class MailReceiver {
             String mail = templateEngine.process("mail", context);
             helper.setText(mail, true);
             javaMailSender.send(msg);
-
+            redisTemplate.opsForHash().put("mail_log", msgId, "lucifer");
+            channel.basicAck(tag, false);
+            logger.info(msgId + ":邮件发送成功");
         } catch (MessagingException e) {
+            channel.basicNack(tag, false, true);
             e.printStackTrace();
             //发送失败，返回日志
             logger.error("邮件发送失败：" + e.getMessage());
         }
-
     }
-
 }
